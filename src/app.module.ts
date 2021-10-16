62bbbc662a99c9429fea9adc3d974bc08007bd41
Marcus <mvsilva1997@gmail.com>
2021-10-15 23:10:26 -0300

    config auth routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UsersModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
